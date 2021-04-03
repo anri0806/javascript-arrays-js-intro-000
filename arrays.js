df4370266df0e20a11c7a92e9049af1bfe1ca2cd
Anri Kawahara <anri0806@hotmail.co.jp>
2021-04-03 01:04:33 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,20 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+
+function addElementToBeginningOfArray(array, element) {
+  return ["foo", ...array];
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift("foo");
+  return array;
+}
+
+function addElementToEndOfArray(array, element) {
+  return [...array, "foo"];
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push("foo");
+  return array;
+}
